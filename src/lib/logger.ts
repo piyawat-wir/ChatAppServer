885207bccff4f7f9fff5bde9f614834002306aea
piyawat-wir <77828805+piyawat-wir@users.noreply.github.com>
2023-03-05 01:18:38 +0700

    Add logger.ts

---
+++ src/lib/logger.ts
@@ -0,0 +1,17 @@
+
+function LoggerFactory(name: string, c: number = 36) {
+	const color = `\x1b[0;${c}m`
+	const reset = `\x1b[0m`
+	return (...args: any[]) => { console.log(`${color}[${name}]${reset}`, ...args) }
+}
+
+export const Log = {
+	server: LoggerFactory('server'),
+	socket: LoggerFactory('socket', 32),
+	middle: LoggerFactory('middle', 33),
+	api: LoggerFactory('api', 34),
+}
+
+console.clear()
+
+export default Log;
