a1eb847003845129497cc0cc6d9b077af425971d
piyawat-wir <77828805+piyawat-wir@users.noreply.github.com>
2023-03-05 16:07:34 +0700

    Update logger.ts

--- src/lib/logger.ts
+++ src/lib/logger.ts
@@ -1,8 +1,14 @@
 
 function LoggerFactory(name: string, c: number = 36) {
-	const color = `\x1b[0;${c}m`
+	const color = `\x1b[2;${c}m`
 	const reset = `\x1b[0m`
-	return (...args: any[]) => { console.log(`${color}[${name}]${reset}`, ...args) }
+	const warn = `\x1b[0;33m`
+	const error = `\x1b[0;31m`
+	const func = (...args: any[]) => { console.log(`${color}[${name}]${reset}`, ...args, reset) }
+	func.log = (...args: any[]) => { console.log(`${color}[${name}]${reset}`, ...args, reset) }
+	func.warn = (...args: any[]) => { console.log(`${color}[${name}]${warn}`, ...args, reset) }
+	func.error = (...args: any[]) => { console.log(`${color}[${name}]${error}`, ...args, reset) }
+	return func;
 }
 
 export const Log = {
