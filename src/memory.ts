db0eba213bd80e894540f8daab3c9043acd78282
piyawat-wir <77828805+piyawat-wir@users.noreply.github.com>
2023-03-05 17:02:34 +0700

    Add memory.ts

---
+++ src/memory.ts
@@ -0,0 +1,18 @@
+import { Room } from "./lib/room";
+import { User } from "./lib/user";
+
+export class Memory {
+
+	private users = new Map<string, User>();
+	private rooms = new Map<string, Room>();
+
+	public room(roomcode: string) { return this.rooms.get(roomcode) }
+	public user(id: string) { return this.users.get(id) }
+
+	public addUser(user: User) { this.users.set(user.id, user) }
+	public removeUser(id: string) { this.users.delete(id) }
+	
+	public addRoom(room: Room) { this.rooms.set(room.roomcode, room) }
+	public removeRoom(roomcode: string) { this.rooms.delete(roomcode) }
+
+}
