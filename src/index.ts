d17f85ab493e2482b405a172c331e2ceb3926919
piyawat-wir <77828805+piyawat-wir@users.noreply.github.com>
2023-03-05 16:28:30 +0700

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,70 +1,94 @@
 import express from 'express'
-import http, { IncomingMessage } from 'http'
+import http from 'http'
 import cors from 'cors'
 import dotenv from 'dotenv'
 import jwt from 'jsonwebtoken'
-import { Server as SocketServer, Socket } from 'socket.io'
-import { AppServerSocket, ErrorCode, SessionData } from './lib/types'
+import { Server as SocketServer } from 'socket.io'
+import { AppServerSocket, ErrorCode, SessionCookieData } from './lib/types'
+import { Log } from './lib/logger'
+import { ExtendedError } from 'socket.io/dist/namespace'
+import { SessionData, SessionStore } from './lib/sessions'
 
 dotenv.config();
 
 const PORT = process.env.PORT || 4000;
 const JWT_TOKEN_KEY = process.env.JWT_TOKEN_KEY || 'defaultsecretkey';
 
+const corsConfig = {
+	origin: process.env.FRONTEND_URI,
+	methods: ['GET', 'POST']
+}
+
 const app = express();
-app.use(cors());
-
 const server = http.createServer(app);
-
 const io = new SocketServer(server, {
-	cors: {
-		origin: process.env.FRONTEND_URI,
-		methods: ['GET', 'POST']
+	cors: corsConfig,
+	connectionStateRecovery: {
+		maxDisconnectionDuration: 2 * 60 * 1000, //2 mins?
+		skipMiddlewares: false
 	}
 });
 
-io.use((socket: AppServerSocket, next) => {
+app.use(cors(corsConfig));
+app.use(express.json());
+
+app.post('/user', (req, res) => { })
+
+type SocketNext = ((err?: ExtendedError | undefined) => void)
+const socketMiddleware = (socket: AppServerSocket, next: SocketNext) => {
 	const { token } = socket.handshake.auth;
 	const error = ErrorCode.InvalidCredential;
 
 	try {
 		if (!token) throw error;
 
-		const session = jwt.verify(token, JWT_TOKEN_KEY) as SessionData;
+		const session = jwt.verify(token, JWT_TOKEN_KEY) as SessionCookieData;
 
 		if (!session) throw error;
 
 		socket.request.session = session;
 	} catch (err) {
-		console.log(`nope! ${socket.id}`);
+		Log.socket.warn(`nope! ${socket.id}`);
 		return next(error)
 	}
 
 	next();
-})
+}
 
-interface SessionSocket {socket: AppServerSocket, roomcode: string}
-const SessionSocketRecord = new Map<string, SessionSocket>();
+io.use(socketMiddleware)
+
+const sessionStore = new SessionStore();
 
 io.on('connection', (socket: AppServerSocket) => {
-	const session = socket.request.session as SessionData;
 
-	console.log(`userid ${session.id}//${socket.id} is connected`);
+	const session = socket.request.session as SessionCookieData;
 
-	if (SessionSocketRecord.has(session.id)) {
-		const {socket: otherSocket} = SessionSocketRecord.get(session.id) as SessionSocket;
-		otherSocket.disconnect();
-		console.log(`disconnected: ${session.id}//${otherSocket.id}`)
+	if (socket.recovered) {
+		Log.socket(`userid ${session.id}//${socket.id} is reconnected`);
 	}
-	SessionSocketRecord.set(session.id, {socket, roomcode: session.roomcode});
+
+	const data = sessionStore.get(session.id);
+	
+	if (data) {
+		const otherSocket = data.getSocket();
+		otherSocket.disconnect();
+		Log.socket(`disconnected: ${session.id}//${otherSocket.id}`)
+		data.setSocket(socket);
+	} else {
+		sessionStore.add(new SessionData(socket, session));
+	}
+	
+	Log.socket(`userid ${session.id}//${socket.id} is connected`);
 
 	socket.on('hello', data => {
-		console.log(`${session.id}//${socket.id}: hello!`);
+		Log.socket(`${session.id}//${socket.id}: hello!`);
 		socket.emit('replyhello');
 	})
 
 })
 
 server.listen(PORT, () => {
-	console.log(`Server is running at localhost:${PORT}`);
+	console.clear()
+	Log.server(`Started running at localhost:${PORT}`);
+	sessionStore.startPruneService();
 })
