22ef4470ed55bf7e7ed923222b956ac98dda5408
piyawat-wir <77828805+piyawat-wir@users.noreply.github.com>
2023-03-05 20:41:21 +0700

    Add expressMiddleware()

--- src/middleware.ts
+++ src/middleware.ts
@@ -2,26 +2,64 @@ import { ExtendedError } from "socket.io/dist/namespace";
 import { AppServerSocket, ErrorCode, SessionCookieData } from "./lib/types";
 import Log from "./lib/logger";
 import jwt from 'jsonwebtoken'
-import dotenv from 'dotenv'
+import { RequestHandler, Request } from 'express'
 
-type SocketNext = ((err?: ExtendedError | undefined) => void)
-export const socketMiddleware = (jwt_key: string) =>
-	(socket: AppServerSocket, next: SocketNext) => {
-		const { token } = socket.handshake.auth;
-		const error = ErrorCode.InvalidCredential;
+export interface AppRequest extends Request {
+	body: {
+		auth?: string
+		data?: Record<string, any>
+	},
+	session?: SessionCookieData
+}
 
-		try {
-			if (!token) throw error;
+interface ValidationParameter {
+	token?: string
+	jwt_key: string
+	req: AppRequest
+}
+function validateSession({ token, jwt_key, req }: ValidationParameter) {
+	const error = ErrorCode.InvalidCredential;
 
-			const session = jwt.verify(token, jwt_key) as SessionCookieData;
+	if (!token) throw error;
 
-			if (!session) throw error;
+	try {
+		const session = jwt.verify(token, jwt_key) as SessionCookieData;
+		if (!session) throw error;
+		req.session = session;
+	}
+	catch (err) { throw error }
+}
 
-			socket.request.session = session;
-		} catch (err) {
-			Log.socket.warn(`nope! ${socket.id}`);
-			return next(error)
+export function expressMiddleware(jwt_key: string): RequestHandler {
+	return (req: AppRequest, res, next) => {
+		const { auth: token } = req.body;
+
+		try { validateSession({ token, jwt_key, req }) }
+		catch (err) {
+			Log.api.warn(`nope! ${req.socket.remoteAddress}`)
+			res.send('No!')
+			return;
 		}
 
 		next();
 	}
+}
+
+type SocketNext = ((err?: ExtendedError | undefined) => void)
+export function socketMiddleware(jwt_key: string) {
+	return (socket: AppServerSocket, next: SocketNext) => {
+		const { token } = socket.handshake.auth;
+
+		try {
+			validateSession({
+				token, jwt_key,
+				req: socket.request as AppRequest
+			})
+		} catch (err: any) {
+			Log.socket.warn(`nope! ${socket.id}`)
+			return next(err)
+		}
+
+		next();
+	}
+}
