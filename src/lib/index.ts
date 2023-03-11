12d266382576edb8a826f79e129286fa274d0067
piyawat-wir <77828805+piyawat-wir@users.noreply.github.com>
2023-03-12 03:49:36 +0700

    Refactor cleanRemoveUser

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,3 +1,6 @@
+import { Memory } from "@/memory";
+import { SessionStore } from "@/lib/sessions";
+
 export function generateID(length: number) {
 	const numeral = "0123456789";
 	const alphabetL = "abcdefghijklmnopqrstuvwxyz";
@@ -10,4 +13,15 @@ export function generateID(length: number) {
 		id += charSet[ci]
 	}
 	return id;
+}
+
+export const cleanRemoveUser = (id: string, sessionStore: SessionStore, memory: Memory) => {
+	const session = sessionStore.get(id);
+	if (!session) return;
+	const room = memory.room(session.roomcode);
+	const user = memory.user(id);
+	if (room && user) room.removeUser(user);
+
+	session.roomcode = '';
+	memory.removeUser(id);
 }
